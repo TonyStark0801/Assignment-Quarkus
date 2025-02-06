f6e0809928653c3f5c73bb3659062cb1aa2becc8
shubham <stark@LT0216787.in.ril.com>
2025-02-07 01:44:00 +0530

    Handled case where q is not provided

--- src/main/java/com/myjio/resource/NewsResource.java
+++ src/main/java/com/myjio/resource/NewsResource.java
@@ -6,6 +6,7 @@ import io.smallrye.common.constraint.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
 
 @Path("/news")
 public class NewsResource {
@@ -17,14 +18,26 @@ public class NewsResource {
     @GET
     @Path("/headlines")
     @Produces(MediaType.APPLICATION_JSON)
-    public NewsResponse getHeadlines(@QueryParam("country") @DefaultValue("us") String country) {
-        return newsService.getHeadlines(country,BASE_HEADLINE_URL);
+    public Response getHeadlines(@QueryParam("country") @DefaultValue("us") String country) {
+        NewsResponse newsResponse = newsService.getHeadlines(country,BASE_HEADLINE_URL);
+        return Response.ok(newsResponse).build();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public NewsResponse getNews (@QueryParam("q") @NotNull String query) {
-        return newsService.getAllNews(query,BASE_URL);
+    public Response getNews(@QueryParam("q") String query) {
+        // Check if 'q' parameter is provided
+        if (query == null || query.trim().isEmpty()) {
+
+            // Throw a custom exception with HTTP 400 Bad Request status
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Query parameter 'q' is required.")
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+
+        NewsResponse newsResponse = newsService.getAllNews(query, BASE_URL);
+        return Response.ok(newsResponse).build();
     }
 
 }
